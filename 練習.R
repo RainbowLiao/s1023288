53c3e41c1dcb67cc6ebf368f3866809e075e7df1
Eric-NB\Eric <lo840813@gmail.com>
2016-09-24 16:39:04 +0800

    R練習

--- 練習.R
+++ 練習.R
@@ -1,9 +1,10 @@
-# this is
-# a
-# f
-# v
+# 可編輯文字紀錄
 
 # 設定工作目錄
-setwd('/Users/Eric/Desktop/凱/Github')
-1 + 1 
+setwd('/Users/Eric/Desktop/凱/生態資訊')
 
+library(swirl)
+swirl()
+is.na (c(1, 2, NA, 3))
+skip()
+bye()
