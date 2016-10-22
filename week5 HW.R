65146905937877c3c9a266a50efbec9392a8fa42
Eric-NB\Eric <lo840813@gmail.com>
2016-10-22 10:48:42 +0800

    10/22 10.48 month complete

--- week5 HW.R
+++ week5 HW.R
@@ -1,5 +1,6 @@
+#  week5 HW aggregate 
+#  all files open _tips: list.files
 
-## list.files
 ## 題目：
 ### 1.計算C0M530(奮起湖)從2006~2015十年的
 ###   a.日均溫、b.日低溫、c.日高溫、d.月均溫、e.月累積降水
@@ -9,28 +10,41 @@
 
 ##### 3.計算最冷月每日最低均溫
 
-# load all of the txt
+# load all of the txt (non-finished)
+{
+library(data.table)
+library(stats)
 
+all_files <- list.files("D:/凱/生態資訊/data/raw/" , pattern = "auto_hr")
 
-data_list <- list.files(path = "D:/凱/生態資訊/data/raw/" , pattern = "auto_hr")
-substr(data_list,1,120)
-filelist <- lapply(data_list, fread)
+substr(all_files,1,120)
 
-for (i in names) {
-  filepath <- file.path('D:/凱/生態資訊/data/raw/')
-  assign(i, read.delim(filepath))
-  }
+read_data <-  function(x){
+  data_open_clean <- fread(x, skip = 74)
+  return(data_open_clean)
+}
+      
+for (i in all_files){
+  temp.data <- fread(i)
+  data <- data.table(rbind(data,temp.data))
+}      
 
+mylist <- lapply(all_files, read_data)
+mydata <- do.call('rbind',mylist)   
 
-  
+}
+---------------------
+    
 
 ##  前置作業
 {
+library(data.table)
+library(stats)  
 setwd('D:/凱/生態資訊/git')
 
  
 # 輸入資料 (tab 可顯示list 路徑)
-library(data.table)
+
 cwb <- fread('D:/凱/生態資訊/data/raw/200601_auto_hr.txt', skip=74 
              , na.strings = c('-9991','-9995','-9996','-9997','-9998','-9999'))
 
@@ -60,6 +74,7 @@ format.Date(C0M530$timestamp, '%Y-%m-%d')
 
 C0M530[, date:= format.Date(timestamp, '%Y-%m-%d')]
 
+
 # 設計funtion,計算mean(注意目前資料讀取為文字檔，需轉換成數值(as.numeric())，
 #                                                   並去除'NA' = na.rm = TRUE)
 
@@ -76,10 +91,9 @@ mean_omit_na <- function(x){
                       FUN = mean_omit_na)
 }
 
-### b.2006年每日最低溫、最高溫Daliy Max/ min tmper (DMaxT/DminT)
+### b.c.2006年每日最低溫、最高溫Daliy Max/ min tmper (DMaxT/DminT)
 {# 寫funtion 判定最大最小值
 
-library(stats)
 Max_T <- function(x){
     max(x, na.rm = TRUE)
 }
@@ -96,6 +110,33 @@ DMaxT <- aggregate(C0M530$TX01, by = list(C0M530$date), FUN = Max_T)
 DminT <- aggregate(C0M530$TX01, by = list(C0M530$date), FUN = min_T)
 }
 
-## 
+### d.2006年1月均溫(month temper (MTM))
+{
+# 新增欄位"month"
+
+C0M530[, month:= format.Date(timestamp, '%Y-%m')]  
+
+#  同a.計算法
+mean_MTM <- aggregate(as.numeric(C0M530$TX01), by = list(C0M530$month),
+                      FUN = mean_omit_na)
+}
+
+### e.2006年1月累積降水(sum)
+{
+# 新增funtion(sum)
+
+sum_omit_na <- function(x){
+  x <- as.numeric(x)
+  return (sum(x, na.rm = TRUE))
+}
+
+
+# 計算總和(sum month rain(SMR))
+
+sum_SMR <- aggregate(as.numeric(C0M530$PP01), by = list(C0M530$month), 
+                      FUN = sum_omit_na) 
+}
+
+
 
   
